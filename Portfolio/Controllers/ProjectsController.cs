bfb97b30a9b4fd8c7ae56e63c109c46d2d0ebec0
Zöldi-Kovács Róbert <zkovacs.robert@gmail.com>
2016-04-29 17:18:56 +0200

    more content

--- Portfolio/Controllers/ProjectsController.cs
+++ Portfolio/Controllers/ProjectsController.cs
@@ -13,5 +13,10 @@ namespace Portfolio.Controllers
         {
             return View();
         }
+
+        public ActionResult Soulmyst()
+        {
+            return View();
+        }
     }
 }
